2812e292229fbf06a818f8a2f71c73d2492e1273
TomicaTT <stefan1612@its.edu.rs>
2016-03-24 19:00:22 +0100

    Added resource to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  root 'posts#index'
+
+  resources :posts, only: [:new, :create, :index]
   resources :sessions, only: [:new, :create, :destroy]
 
   get 'login' => 'sessions#new'
